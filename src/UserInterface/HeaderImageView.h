2c05bbdde804efb1dd839a9c6e29f10c6818f9f4
mdbergmann <>
2008-08-25 06:07:24 +0000

    added CommentaryView xib, header image view

---
+++ src/UserInterface/HeaderImageView.h
@@ -0,0 +1,19 @@
+//
+//  HeaderImageView.h
+//  MacSword2
+//
+//  Created by Manfred Bergmann on 24.08.08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface HeaderImageView : NSView {
+    // the image
+    NSImage *bgImage;
+}
+
+@property (retain, readwrite) NSImage *bgImage;
+
+@end
---
+++ src/UserInterface/HeaderImageView.m
@@ -0,0 +1,35 @@
+//
+//  HeaderImageView.m
+//  MacSword2
+//
+//  Created by Manfred Bergmann on 24.08.08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "HeaderImageView.h"
+
+
+@implementation HeaderImageView
+
+@synthesize bgImage;
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+        self.bgImage = [NSImage imageNamed:@"header_gradient.png"];
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)rect {
+    // Drawing code here.
+    NSRect tmp = [self bounds];
+    int repeat = (tmp.size.width / 5) + 1;
+    for(int i = 0;i < repeat;i++) {
+        [bgImage drawInRect:tmp fromRect:[self bounds] operation:NSCompositeSourceOver fraction:1.0];
+        tmp.origin.x += 5;
+    }
+}
+
+@end
