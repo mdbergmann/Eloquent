bef9624634dbbefe900fe2c37c958e8a43384a4f
mdbergmann <>
2009-04-03 08:26:45 +0000

    replaced perl scripts with macruby. also fixed some bugs and added new build of Sword library

---
+++ get_bundle_version.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env macruby
+
+info_dict = NSDictionary.dictionaryWithContentsOfFile("./Info.plist")
+if !info_dict 
+  puts "Couldn't load Info.plist"
+end
+
+print info_dict["CFBundleVersion"]
