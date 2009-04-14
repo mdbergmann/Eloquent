587def8183fba337426424ee048f78633150b54e
mdbergmann <>
2009-04-14 17:08:35 +0000

    added test case

---
+++ test/SwordListKeyTest.m
@@ -0,0 +1,23 @@
+//
+//  SwordListKeyTest.m
+//  MacSword2
+//
+//  Created by Manfred Bergmann on 10.04.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "SwordListKeyTest.h"
+
+
+@implementation SwordListKeyTest
+
+- (void)testContainsKey {
+    
+    SwordListKey *lk = [SwordListKey listKeyWithRef:@"Gen 1:1-5"];
+    SwordVerseKey *vk = [SwordVerseKey verseKeyWithRef:@"Gen 1:3"];
+    BOOL result = [lk containsKey:vk];
+    
+    STAssertTrue(result);
+}
+
+@end
