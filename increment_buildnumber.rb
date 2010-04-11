4367f9c356a6663ffee39c632c61edebb6059369
Manfred Bergmann <mbergmann@Candit.local>
2010-04-11 22:07:05 +0100

    last commit in 2.2.

--- increment_buildnumber.rb
+++ increment_buildnumber.rb
@@ -1,12 +1,6 @@
 #!/usr/bin/env macruby
 
-# the prefix is incremented for every minor/major version number increment
-# the actual number begins at 1
-prefix = "a"
-
 buildstring = File.new("buildnumber", "r").gets
-splitnum = buildstring.split(prefix)
-number = splitnum[splitnum.length-1].to_i
+number = buildstring.to_i
 number += 1
-buildnumber = prefix + number.to_s
-File.new("buildnumber", "w+").write(buildnumber)
+File.new("buildnumber", "w+").write(number.to_s)
