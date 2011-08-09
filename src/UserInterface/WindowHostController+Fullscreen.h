006baf6e87d05eb88528260c8c1473c9ee21cf2f
Manfred Bergmann <>
2011-08-09 22:20:22 +0200

    file cleanup

---
+++ src/UserInterface/WindowHostController+Fullscreen.h
@@ -0,0 +1,17 @@
+//
+//  WindowHostController+Fullscreen.h
+//  Eloquent
+//
+//  Created by Manfred Bergmann on 19.02.10.
+//  Copyright 2010 Software by MABE. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <WindowHostController.h>
+
+
+@interface WindowHostController (Fullscreen)
+
+- (BOOL)isFullScreenMode;
+
+@end
---
+++ src/UserInterface/WindowHostController+Fullscreen.m
@@ -0,0 +1,48 @@
+//
+//  WindowHostController+Fullscreen.m
+//  Eloquent
+//
+//  Created by Manfred Bergmann on 19.02.10.
+//  Copyright 2010 Software by MABE. All rights reserved.
+//
+
+#import "WindowHostController+Fullscreen.h"
+#import "WindowHostController+SideBars.h"
+#import "ToolbarController.h"
+
+
+@implementation WindowHostController (Fullscreen)
+
+- (BOOL)isFullScreenMode {
+    return inFullScreenMode;
+}
+
+- (void)windowWillEnterFullScreen:(NSNotification *)notification {
+    CocoLog(LEVEL_DEBUG, @"going to fullscreen");
+    
+    inFullScreenTransition = YES;
+}
+
+- (void)windowDidEnterFullScreen:(NSNotification *)notification {
+    CocoLog(LEVEL_DEBUG, @"gone to fullscreen");
+    
+    inFullScreenTransition = NO;
+    [self forceReload:nil];
+    inFullScreenMode = YES;
+}
+
+- (void)windowWillExitFullScreen:(NSNotification *)notification {
+    CocoLog(LEVEL_DEBUG, @"leaving fullscreen");
+    
+    inFullScreenTransition = YES;
+}
+
+- (void)windowDidExitFullScreen:(NSNotification *)notification {
+    CocoLog(LEVEL_DEBUG, @"left fullscreen");
+    
+    inFullScreenTransition = NO;
+    [self forceReload:nil];
+    inFullScreenMode = NO;
+}
+
+@end
